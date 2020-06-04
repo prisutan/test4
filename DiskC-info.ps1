7c8de253ffa6005d6ecfe1aeaba4b92178e4ef08
prisutan <goran.bgd@gmail.com>
2020-06-04 17:05:44 +0200

    readme changed

--- DiskC-info.ps1
+++ DiskC-info.ps1
@@ -1,3 +1,7 @@
+#skripta koja proverava prostor na lokalnim diskovima
 Get-WmiObject -class Win32_LogicalDisk -computername localhost -filter "drivetype=3" |
 Sort-Object -property DeviceID |
-Format-Table -property DeviceID, @{label='FreeSpace(MB)';expression={$_.FreeSpace / 1MB -as [int]}}, @{label='Size(GB)';expression={$_.Size / 1GB -as [int]}}, @{label='%Free';expression={$_.FreeSpace / $_.Size * 100 -as [int]}}
+Format-Table -property DeviceID, 
+@{label='FreeSpace(GB)';expression={$_.FreeSpace / 1GB -as [int]}}, 
+@{label='Size(GB)';expression={$_.Size / 1GB -as [int]}}, 
+@{label='%Free';expression={$_.FreeSpace / $_.Size * 100 -as [int]}}
