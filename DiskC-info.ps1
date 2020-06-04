06601c61c744eff52e90fcc1f2117b9422d7a21c
prisutan <goran.bgd@gmail.com>
2020-06-04 17:47:45 +0200

    initial commit3

--- DiskC-info.ps1
+++ DiskC-info.ps1
@@ -1,5 +1,8 @@
 #skripta koja proverava prostor na lokalnim diskovima
-Get-WmiObject -class Win32_LogicalDisk -computername localhost -filter "drivetype=3" |
+param ( $computername="localhost"
+)
+Get-WmiObject -class Win32_LogicalDisk -computername $computername `
+-filter "drivetype=3" |
 Sort-Object -property DeviceID |
 Format-Table -property DeviceID, 
 @{label='FreeSpace(GB)';expression={$_.FreeSpace / 1GB -as [int]}}, 
